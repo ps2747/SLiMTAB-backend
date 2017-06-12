a7c9f716d262bc566fdf98617bc78dd2c3417c1a
tim37021 <tim37021@gmail.com>
2017-06-12 23:53:03 +0800

    Improved performance

--- AudioSynth.py
+++ AudioSynth.py
@@ -7,13 +7,12 @@ class AudioSynth:
         self.sr = sr
         self.data = np.zeros(44100)
         self.start_time = 0
+        self.noise_pool = {}
 
     def gen_signal(self, start, N, freq, phase=0):
         n = int(self.sr/freq*2)
-        noise = np.zeros(n)
-        theta0 =int(phase*n)
-        for i in range(n):
-            noise[i] = np.random.random()-0.5
+        noise = np.random.rand(n)-0.5
+        theta0 = int(phase*n)
         for i in range(N):
             noise[(i+n-theta0)%n] = 0.5*(noise[(i+n+1-theta0)%n]+noise[(i+n-theta0)%n])
             self.data[i+start] += noise[(i+n-theta0)%n]
@@ -22,7 +21,7 @@ class AudioSynth:
         self.data = np.zeros(int((l[-1]['time']+l[-1]['duration'])*self.sr)+100)
         for nt in l:
             freq = 82.41*np.power(1.0594630943592953, nt['note']-28)
-            self.gen_signal(int(nt['time']*self.sr), int(nt['duration']*self.sr), freq, np.random.random())
+            self.gen_signal(int(nt['time']*self.sr), int(nt['duration']*self.sr), int(freq), np.random.random())
 
     def getCurrentTime(self):
         return time.perf_counter() - self.start_time
